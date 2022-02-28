1c47761a898c8f1ccea40bce98ec3a1666b82a0e
mazenmdimegh <mazenmdimagh@gmail.com>
2022-02-28 16:04:04 +0100

    update name

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'Clicker',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.purple,
         ),
         home: const HomeScreen());
   }
